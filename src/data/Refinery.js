1b285c2e0d65dcfd04050373c21f504216bc88d6
Jaclyn Hoglund <jackie.o.hoglund@gmail.com>
2021-12-10 08:51:10 -0700

    Added refineries

---
+++ src/data/Refinery.js
@@ -0,0 +1,101 @@
+var line9Refineries = {
+  "type" : "FeatureCollection",
+  "crs" : {
+    "type" : "name",
+    "properties" : {
+      "name" : "EPSG:3857"
+    }
+  },
+  "features" : [
+    {
+      "type" : "Feature",
+      "id" : 22,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -8910913.0021000002,
+          5287115.3541999981
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 22,
+        "FEATURE_ID" : "R0006",
+        "NAME_EN" : "Imperial Oil Nanticoke Refinery",
+        "CREATED_BY" : "HOGLJACK",
+        "CREATED_DATE" : 1610463884000,
+        "CREATED_SOURCE" : "Enbridge Pipelines Inc., Application for Canadian Mainline Contracting, RH-001-2020 File No.: OF-Tolls-Group-E101-2019-02 02,  CER IR 1.10.a Attachment - Enbridge Mainline Connected Refineries (CO6801-15) ",
+        "REVISED_BY" : "HOGLJACK",
+        "REVISED_DATE" : 1637004382000,
+        "REVISED_COMMENT" : "Revised location based on google maps/imagery",
+        "REFINERY_LOC" : "Nanticoke, Ontario",
+        "OWNERS_EN" : "IOL",
+        "MOC" : 113000,
+        "CONNECTED_LINES" : "11",
+        "ONLY_S_EN" : "Yes",
+        "NAME_FR" : null,
+        "OWNERS_FR" : null,
+        "ONLY_S_FR" : null
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 32,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -8184512.6262999997,
+          5723517.7388999984
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 32,
+        "FEATURE_ID" : "R0016",
+        "NAME_EN" : "Suncor Montreal Refinery",
+        "CREATED_BY" : "HOGLJACK",
+        "CREATED_DATE" : 1635191668000,
+        "CREATED_SOURCE" : "Google Maps",
+        "REVISED_BY" : "HOGLJACK",
+        "REVISED_DATE" : 1636995723000,
+        "REVISED_COMMENT" : null,
+        "REFINERY_LOC" : "Montreal, Quebec",
+        "OWNERS_EN" : "Suncor",
+        "MOC" : null,
+        "CONNECTED_LINES" : "9",
+        "ONLY_S_EN" : null,
+        "NAME_FR" : null,
+        "OWNERS_FR" : null,
+        "ONLY_S_FR" : null
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 33,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -7924904.6585000008,
+          5903085.9506999999
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 33,
+        "FEATURE_ID" : "R0017",
+        "NAME_EN" : "Valero Jean Gaulin Refinery",
+        "CREATED_BY" : "HOGLJACK",
+        "CREATED_DATE" : 1635195528000,
+        "CREATED_SOURCE" : "Google Maps",
+        "REVISED_BY" : "HOGLJACK",
+        "REVISED_DATE" : 1637878119000,
+        "REVISED_COMMENT" : null,
+        "REFINERY_LOC" : "Quebec City, Quebec",
+        "OWNERS_EN" : "Valero",
+        "MOC" : null,
+        "CONNECTED_LINES" : "9",
+        "ONLY_S_EN" : "No",
+        "NAME_FR" : null,
+        "OWNERS_FR" : null,
+        "ONLY_S_FR" : null
+      }
+    }
+  ]
+}
