2325d59e94fe2c721026fae54de5d8f34eea7d24
Jaclyn Hoglund <jackie.o.hoglund@gmail.com>
2021-12-24 08:43:24 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function leafletBaseMap(config) {
   const map = new L.map(config.div).setView(
     config.initZoomTo,
     config.initZoomLevel,
-  ).maxZoom(5);
+  );
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}", {
     foo: "bar",
     attribution:
